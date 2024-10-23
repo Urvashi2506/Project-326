33a4913924374e8162a7ad22955bf3324c9cbfd6
URVASHI YADAV <urvashiyadav2506@gmail.com>
2024-10-23 17:57:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
